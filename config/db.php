45151094c2c9d5fbb663e1776edb387f5bb62bd9
TejiriUkri <tejiriukri1@gmail.com>
2025-11-08 01:01:38 +0100

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,25 @@
+<?php 
+// ob_start();
+
+// session_start();
+
+
+// defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR); 
+
+
+
+// defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front"); 
+
+// defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+
+// defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", __DIR__ . DS . "upload");
+
+//DB params
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "office_attendance");
+
+require_once("pdo_db.php");
+// require_once("../resources/Function.php");
+
